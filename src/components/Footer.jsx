ad4d861512fb150fcb0789debc0d397cde8d22ca
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-15 21:26:10 +0800

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,32 @@
-function Footer({ footerAPI: { title, links } }) {
+function Footer({ footerAPI: { titles, links } }) {
   return (
     <>
-      <footer>
-        <div>
-          <div>
-            {title.map((val, i) => (
-              <div key={i} className="">
-                <h1>{val.title}</h1>
+      <footer className="bg-theme pt-7 pb-5">
+        <div className="nike-container text-slate-200">
+          <div className="grid items-start grid-cols-3 max-w-2xl w-full m-auto md:max-w-none md:gap-5">
+            {titles.map((val, i) => (
+              <div key={i} className="grid items-center">
+                <h1 className="text-lg lg:text-base md:text-sm uppercase font-semibold">
+                  {val.title}
+                </h1>
               </div>
             ))}
             {links.map((list, i) => (
-              <ul key={i}>
+              <ul key={i} className="grid items-center gap-1">
                 {list.map((link, i) => (
-                  <li key={i}>{link.link}</li>
+                  <li key={i} className="text-sm sm:text-sm">
+                    {link.link}
+                  </li>
                 ))}
               </ul>
             ))}
+          </div>
+          <div className="mt-5 text-center">
+            <p className="text-sm md:text-center">
+              Copyright<sup className="text-base font-bold"> &copy;</sup> All
+              Reserved Rights
+              <span className="font-semibold"> MJCODES</span>
+            </p>
           </div>
         </div>
       </footer>
