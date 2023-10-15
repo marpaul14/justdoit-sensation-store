8c6a051b63fb70a4a033afdf8b9f3a9eb79b0a37
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-15 18:22:53 +0800

    styling flexcontent component

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <main className="flex flex-col gap-16 relative">
         <Hero heroapi={heroapi} />
         <Sales endpoint={popularsales} ifExists />
-        <FlexContent endpoint={highlight} />
+        <FlexContent endpoint={highlight} ifExists />
         <Sales endpoint={toprateslaes} />
         <FlexContent endpoint={sneaker} />
       </main>
--- src/components/FlexContent.jsx
+++ src/components/FlexContent.jsx
@@ -1,23 +1,43 @@
-function FlexContent({ endpoint: { title, heading, text, img, btn, url } }) {
+function FlexContent({
+  ifExists,
+  endpoint: { title, heading, text, img, btn, url },
+}) {
   return (
     <>
       <div
-        className={`flex items-center justify-between lg:flex-col lg:justify-center nike-container`}
+        className={`flex items-center justify-between lg:flex-col lg:justify-center nike-container ${
+          ifExists ? 'flex-row-reverse' : 'flex-row'
+        }`}
       >
-        <div className="max-w-lg lg:max-w-none w-full md:text-center">
+        <div className="max-w-lg lg:max-w-none w-full md:text-center grid items-center lg:justify-items-center">
           <h1 className="text-4xl sm:text-3xl font-bold text-gradient">
             {heading}
           </h1>
-          <h1>{title}</h1>
-          <p>{text}</p>
-          <a href={url}>
-            <button type="button" className="">
+          <h1 className="text-5xl lg:text-4xl md:text-3xl sm:text-2xl font-bold text-slate-900 filter drop-shadow-lg">
+            {title}
+          </h1>
+          <p className="xl:text-sm my-4 text-slate-900">{text}</p>
+          <a
+            href={url}
+            className="flex items-center"
+            target={'_blank'}
+            rel="noreferrer"
+            role="button"
+          >
+            <button
+              type="button"
+              className="button-theme bg-slate-900 shadow-slate-900 text-slate-100 py-1.5"
+            >
               {btn}
             </button>
           </a>
         </div>
-        <div className="">
-          <img src={img} alt={`img/${heading}`} className="" />
+        <div className="flex items-center justify-center max-w-xl relative lg:max-w-none w-full">
+          <img
+            src={img}
+            alt={`img/${heading}`}
+            className={`w-auto objective-fill transitions-theme`}
+          />
         </div>
       </div>
     </>
