cd135c069a70b394cd19632c45b3b6be5cd3f421
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-17 21:57:23 +0800

    cartempty component added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,8 +5,12 @@ import CartItem from './Cart/CartItem';
 function Cart() {
   return (
     <>
-      <div>
-        <div>
+      <div
+        className={`fixed top-0 left-0 right-0 bottom-0 blur-effect-theme w-full h-screen opacity-100 z-[250]`}
+      >
+        <div
+          className={`blur-effect-theme h-screen max-w-xl w-full absolute right-0`}
+        >
           <CartCount />
           <CartEmpty />
           <CartItem />
