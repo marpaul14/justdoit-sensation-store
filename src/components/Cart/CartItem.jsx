621df0fc02aa3a486a4d0abe5fd6210584a676e0
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-23 11:40:33 +0800

    cartItems component done

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -7,36 +7,53 @@ function CartItem({
     <>
       <div className="flex items-center justify-between w-full px-5">
         <div className="flex items-center gap-5">
-          <div className={`bg-gradient-to-b ${color} ${shadow}`}>
+          <div
+            className={`bg-gradient-to-b ${color} ${shadow} relative rounded p-3 hover:scale-105 transition-all duration-75 ease-in-out grid items-center`}
+          >
             <img
               src={img}
               alt={`img/cart-item/${id}`}
               className="w-36 h-auto object-fill lg:w-28"
             />
           </div>
-          <div className="">
-            <div className="">
-              <h1>{title}</h1>
-              <p>{text}</p>
+          <div className="grid items-center gap-4">
+            <div className="grid items-center leading-none">
+              <h1 className="font-medium text-lg text-slate-900 lg:text-sm">
+                {title}
+              </h1>
+              <p className="text-sm text-slate-800 lg:text-xs">{text}</p>
             </div>
-            <div className="">
-              <button type="button" className="">
-                <MinusIcon className="w-5 h-5" />
+            <div className="flex items-center justify-around w-full">
+              <button
+                type="button"
+                className="bg-theme-cart rounded w-6 h-6 lg:w-5 lg:h-5 flex items-center justify-center active:scale-90"
+              >
+                <MinusIcon className="w-5 h-5 lg:w-4 lg:h-4 text-white stroke-[2]" />
               </button>
-              <div className="">{cartQuantity}</div>
-              <button>
-                <PlusIcon className="w-5 h-5" />
+              <div className="bg-theme-cart rounded text-white font-medium lg:text-xs w-7 h-6 lg:h-5 lg:w-6 flex items-center justify-center">
+                {cartQuantity}
+              </div>
+              <button
+                type="button"
+                className="bg-theme-cart rounded w-6 h-6 lg:w-5 lg:h-5 flex items-center justify-center active:scale-90"
+              >
+                <PlusIcon className="w-5 h-5 lg:w-4 lg:h-4 text-white stroke-[2]" />
               </button>
             </div>
           </div>
         </div>
-        <div className="">
-          <div className="">
-            <h1>{price * cartQuantity}</h1>
+        <div className="grid items-center gap-5">
+          <div className="grid items-center justify-center">
+            <h1 className="text-lg lg:text-base text-slate-900 font-medium">
+              ${price * cartQuantity}
+            </h1>
           </div>
-          <div className="">
-            <button type="button" className="">
-              <TrashIcon className="w-5 h-5" />
+          <div className="grid items-center justify-center">
+            <button
+              type="button"
+              className="bg-theme-cart rounded p-1 lg:p-0.5 grid items-center justify-items-center"
+            >
+              <TrashIcon className="w-5 h-5 text-white " />
             </button>
           </div>
         </div>
